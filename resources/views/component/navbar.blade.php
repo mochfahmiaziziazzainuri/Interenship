b0675243352e796baad068c7bc9215554169b8a0
haqiq <ngabuburits018@gmail.com>
2023-12-20 13:16:33 +0700

    perubahan

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="container-fluid bg-white sticky-top">
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-white navbar-light p-lg-0">
-            <a href="index.html" class="navbar-brand d-lg-none">
-                <h1 class="fw-bold m-0">GrowMark</h1>
+            <a href="{{ route('home') }}" class="navbar-brand d-lg-none">
+                <h1 class="fw-bold m-0">INTERNSHIP</h1>
             </a>
             <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse"
                 data-bs-target="#navbarCollapse">
@@ -15,9 +15,6 @@
                     <a href="{{ route('service') }}" class="nav-item nav-link" style="color:{{ Request::routeIs('service') ? 'blue' : '' ;}}">Service</a>
                     <a href="{{ route('project') }}" class="nav-item nav-link" style="color:{{ Request::routeIs('project') ? 'blue' : '' ;}}">Project</a>
                     <a href="{{ route('contact') }}" class="nav-item nav-link" style="color:{{ Request::routeIs('contact') ? 'blue' : '' ;}}">Contact</a>
-                </div>
-                <div class="ms-auto d-none d-lg-block">
-                    <a href="" class="btn btn-primary rounded-pill py-2 px-3">Get A Quote</a>
                 </div>
             </div>
         </nav>
