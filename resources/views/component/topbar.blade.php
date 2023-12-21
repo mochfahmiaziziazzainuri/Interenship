8c236d7a74d9376ea9a2c7a783e1d8b48bf5dcc6
haqiq <ngabuburits018@gmail.com>
2023-12-21 10:47:10 +0700

    tambahan login

--- resources/views/component/topbar.blade.php
+++ resources/views/component/topbar.blade.php
@@ -5,7 +5,7 @@
                 <h2 class="text-white fw-bold m-0">INTERNSHIP</h2>
             </a>
             <div class="ms-auto d-flex align-items-center">
-                <small class="ms-4"><i class="fa fa-map-marker-alt me-3"></i>Jl.Pangurakan No.1, Ngupasan, Kec. Gondomanan, Kota Yogyakarta, Daerah Istimewa Yogyakarta</small>
+                <small class="ms-4"><i class="fa fa-map-marker-alt me-3"></i>Kota Yogyakarta, Daerah Istimewa Yogyakarta</small>
                 <small class="ms-4"><i class="fa fa-envelope me-3"></i>email</small>
                 <small class="ms-4"><i class="fa fa-phone-alt me-3"></i>No. telephone</small>
                 <div class="ms-3 d-flex">
